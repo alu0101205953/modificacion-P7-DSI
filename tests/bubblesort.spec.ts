b68b2963c6b821068f0b5f56c8fbccfafe673770
Iris Pereira <alu0101205953@ull.edu.es>
2021-04-14 10:55:01 +0100

    test changed

--- tests/bubblesort.spec.ts
+++ tests/bubblesort.spec.ts
@@ -7,6 +7,6 @@ let bSort = new bubbleSort();
 
 describe('Bubble Sort tests', () => {
   it('Order an array using bubble sort method', () => {
-     expect(bSort.execute(data).toString).to.be.equal([1, 2, 4, 5, 7, 8, 9].toString);
+     expect(bSort.execute(data)).to.be.eql([1, 2, 4, 5, 7, 8, 9]);
   });
 });
--- tests/mergesort.spec.ts
+++ tests/mergesort.spec.ts
@@ -7,6 +7,6 @@ let mSort = new mergeSort();
 
 describe('Merge Sort tests', () => {
   it('Order an array using merge sort method', () => {
-     expect(mSort.execute(data).toString).to.be.equal([1, 2, 4, 5, 7, 8, 9].toString);
+     expect(mSort.execute(data)).to.be.eql([1, 2, 4, 5, 7, 8, 9]);
   });
 });
